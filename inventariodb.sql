f027d1d1d370778cb7266a9ea4b422c56313e918
Mrquez <cmarquezmoreno@gmail.com>
2023-06-27 11:38:54 -0600

    BD

--- inventariodb.sql
+++ inventariodb.sql
@@ -3,9 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2023 a las 19:55:48
+-- Tiempo de generación: 27-06-2023 a las 19:37:39
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -15,8 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-CREATE SCHEMA IF NOT EXISTS `inventariodb` DEFAULT CHARACTER SET utf8 ;
-USE `inventariodb` ;
+
 --
 -- Base de datos: `inventariodb`
 --
@@ -32,18 +32,43 @@ CREATE TABLE `articulo` (
   `nombre` varchar(45) NOT NULL,
   `fecha_creacion` date NOT NULL,
   `fecha_modificacion` date NOT NULL,
-  `cantidad` int(11) NOT NULL
+  `cantidad` int(11) NOT NULL,
+  `Descripcion` varchar(400) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `articulo`
 --
 
-INSERT INTO `articulo` (`idarticulo`, `nombre`, `fecha_creacion`, `fecha_modificacion`, `cantidad`) VALUES
-(1, 'CPU', '0000-00-00', '0000-00-00', 1),
-(2, '11:49', '0000-00-00', '0000-00-00', 1),
-(3, '11:51', '0000-00-00', '0000-00-00', 1),
-(4, '11:54', '2023-06-23', '2023-06-23', 1);
+INSERT INTO `articulo` (`idarticulo`, `nombre`, `fecha_creacion`, `fecha_modificacion`, `cantidad`, `Descripcion`) VALUES
+(5, 'CPU', '2023-06-23', '2023-06-23', 1, ''),
+(6, 'Monitor', '2023-06-23', '2023-06-23', 10, ''),
+(7, 'Mouse', '2023-06-23', '2023-06-23', 3, ''),
+(9, 'CPU', '2023-06-27', '2023-06-27', 12, 'A'),
+(10, '', '2023-06-27', '0000-00-00', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitud`
+--
+
+CREATE TABLE `solicitud` (
+  `idSolicitud` int(11) NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `hora_inicio` varchar(10) NOT NULL,
+  `hora_fin` varchar(10) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha_solicitada` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `solicitud`
+--
+
+INSERT INTO `solicitud` (`idSolicitud`, `fecha_creacion`, `hora_inicio`, `hora_fin`, `descripcion`, `fecha_solicitada`) VALUES
+(1, '2023-06-27', '11:24 AM', '11:28 AM', 'sfvzc', '2023-06-28'),
+(2, '2023-06-27', '11:25 AM', '11:29 AM', 'zsdgvdfg', '2023-06-29');
 
 -- --------------------------------------------------------
 
@@ -59,6 +84,15 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nom_usuario`, `num_empleado`, `contrasena`) VALUES
+(1, 'Claudio', 10101010, '10101010'),
+(2, 'Jesus Eloy', 120846, '120846'),
+(3, 'Claudio Mqz', 101010, '101010');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -67,6 +101,12 @@ CREATE TABLE `usuario` (
 --
 ALTER TABLE `articulo`
   ADD PRIMARY KEY (`idarticulo`);
+
+--
+-- Indices de la tabla `solicitud`
+--
+ALTER TABLE `solicitud`
+  ADD PRIMARY KEY (`idSolicitud`);
 
 --
 -- Indices de la tabla `usuario`
@@ -82,13 +122,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud`
+--
+ALTER TABLE `solicitud`
+  MODIFY `idSolicitud` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
