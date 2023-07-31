4433a2c6056573a6b23871d051c19c3b942c3469
Mrquez <cmarquezmoreno@gmail.com>
2023-07-31 08:45:45 -0600

    verificacion de nuemero de empleado

--- Proyecto/js/register.js
+++ Proyecto/js/register.js
@@ -7,9 +7,8 @@ Contiene las funciones necesarias para mostrar hacer el registro validado de usu
 
 function validateForm(){
     var x = document.forms["registrar"]["num_empleado"].value;
-    var x1 = document.forms["registrar"]["contrasena"].value;
-    var x2 = document.forms["registrar"]["nom_usuario"].value;
-    var x3 = document.forms["registrar"]["contrasena2"].value;
+    var x1 = document.forms["registrar"]["nom_usuario"].value;
+    var x2 = document.forms["registrar"]["num_empleado2"].value;
 
 
       if(x==""|| x1=="" || x2==""){
@@ -20,17 +19,11 @@ function validateForm(){
         return false;
 
       }
-      else if (x != x1){
+      else if (x != x2){
         document.getElementById('condif').style.display = 'block';
         document.getElementById('error-message2').style.display = 'none';
         document.getElementById('error-message1').style.display = 'none';
         return false;
       }
-      else if(x1 != x3)
-      {
-        document.getElementById('error-message1').style.display = 'none';
-        document.getElementById('error-message2').style.display = 'block';
-        document.getElementById('condif').style.display = 'none';
-        return false;
-      }
+
     } 
